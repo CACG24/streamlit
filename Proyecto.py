dc7c60dfc68a2fbe64a2aabee15974114c0ae4be
CACG24 <156930996+CACG24@users.noreply.github.com>
2024-11-26 23:38:56 -0600

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -631,5 +631,7 @@ st.image(Grafica_2D2, width = 500)
 st.image(Grafica_2D3, width = 500)
 
 #Datos de los clusters
+st.divider()
+st.subheader('Información de los clusters')
 cantidadGrupo = pd.read_csv('./Datos/MiBici/cantidadGrupo.csv', index_col=0)
 st.dataframe(cantidadGrupo.head(10))
